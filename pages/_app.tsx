e866c75678feea8512e92a086ff3748a27f92f20
Oleksandr “Sasha” Mitichkin <infoformiti@gmail.com>
2025-02-17 14:33:18 +0000

    Changed from vercel to cloudfare analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
       <SpeedInsights />
+      <Script
+        strategy="afterInteractive"
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon={JSON.stringify({ token: process.env.NEXT_PUBLIC_CLOUDFLARE_ANALYTICS_KEY })}
+      />
     </>
   );
 }
