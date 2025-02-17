983bedb3b9bd3f063e037ccf506363b5a62fcd94
Oleksandr “Sasha” Mitichkin <infoformiti@gmail.com>
2025-02-17 15:21:43 +0000

    Improvements for google search

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import Script from 'next/script';
@@ -6,6 +7,20 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+              "@type": "Website",
+              "name": "No-Show Bus Tracker / Report Tool",
+              "url": "https://nobus.omiti.net",
+              "description": "Report missing buses in Cork in real-time."
+            }),
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
       <SpeedInsights />
       <Script
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,11 @@ export default function Home() {
 
   return (
     <div>
-      <Head>
-        <title>No-Show Bus Tracker - Cork</title>
+      <Head>      
+        <title>No-Show Bus Tracker / Report - Cork</title>
+        <meta name="description" content="Report no-show buses in Cork with real-time updates and insights. Stay informed and plan your travel better!" />
+        <meta name="keywords" content="no-show bus, no bus cork, report canceled bus Cork, Cork bus tracker, missing buses, public transport Cork, report bus, cork" />
+        <meta name="author" content="Oleksandr Mitichkin" />
       </Head>
       <Navbar />
       <main className={styles.main}>        
